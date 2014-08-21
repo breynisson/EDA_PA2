9cfbd86f2fc15764710d292f12252e5120e766ab
breynisson <bjorgvin.reynisson@gmail.com>
2014-08-21 13:44:39 +0000

    Add plot.

--- plot1.R
+++ plot1.R
@@ -2,8 +2,13 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 library(plyr)
+# Group and Sum the Emission data by year:
 emissions_by_year<-aggregate(Emissions ~ year, data=NEI, sum)
-plot(emissions_by_year)
-barplot(emissions_by_year$Emissions, col="red", 
+
+# Plot PM2.5 Emission in Million Tons vs Year and save as plot1.png:
+png("plot1.png")
+barplot(emissions_by_year$Emissions/1000000, col="red", 
         names.arg=emissions_by_year$year, 
-        main=bquote("Total "~PM[2.5]~" Emissions in the US per year"))
+        main=bquote("Total "~PM[2.5]~" Emissions in the US per year"),
+        xlab="Year", ylab="Million Tons")
+dev.off()
